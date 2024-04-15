77f51d2583aca87185522ae69878b155cb3cd7e4
beishenbaevasaikal <beishenbaevasaikal@gmail.com>
2024-04-15 18:28:49 +0600

    конец

--- eni.py
+++ eni.py
@@ -4,29 +4,21 @@ import turtle
 t = turtle.Turtle()
 s = turtle.Screen()
 s.bgcolor("black")
+t.pencolor("blue")
 
-turtle.pensize(2)
+a = 0
+b = 0
+t.speed(0)
+t.penup()
+t.goto(0, 200)
+t.pendown()
+while (True):
+    t.forward(a)
+    t.right(b)
+    a += 3
+    b += 1
+    if b == 210:
+        break
+    t.hideturtle()
 
-
-# To design curve
-def curve():
-    for i in range(200):
-        t.right(1)
-        t.forward(1)
-
-
-t.speed(3)
-t.color("red", "pink")
-
-t.begin_fill()
-t.left(140)
-t.forward(111.65)
-curve()
-
-t.left(120)
-curve()
-t.forward(111.65)
-t.end_fill()
-t.hideturtle()
-
-turtle.mainloop()
+turtle.done()
